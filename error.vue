3657d4753f092bd228b1971c1c835e8b2445a90b
Eric Russell <Erictim.Russell@live.com>
2023-01-20 13:21:29 -0700

    error handling page

---
+++ error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-center card">
+    <p class="my-7 text-7xl font-bold">{{ error.statusCode }}</p>
+    <p class="text-6xl">Oooops.</p>
+    <p class="mt-7">{{ error.message }}</p>
+    <button @click="handleClearError" class="btn my-7">Go Home...</button>
+  </div>
+</template>
+
+<script setup>
+defineProps(['error'])
+
+const handleClearError = () => clearError({ redirect: '/' })
+</script>
+
+<style scoped>
+
+</style>
